a874c4275798ef3348bef463d5d42eac824c0a6f
kin_darnakes <kindenene@gmail.com>
2021-06-05 20:28:18 +0200

    init

--- App.js
+++ App.js
@@ -3,12 +3,15 @@ import 'react-dom';
 
 class Hola extends React.Component {
     render() {
-      return(
-        <h1>Hola mundo desde React</h1>
-      )
+        return (
+            <div>
+                <h1>Hola mundo desde React</h1>
+                <p></p>
+            </div>
+        )
     }
- }
+}
 
- let container = document.getElementById('React');
+let container = document.getElementById('React');
 let component = <Hola />;
 ReactDOM.render(component, container);
