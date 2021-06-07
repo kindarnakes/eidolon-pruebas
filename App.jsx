fe6ffd361f00374b44a05ca636ad0bb7b5cd9f59
kindarnakes <kindenene@gmail.com>
2021-06-08 00:06:48 +0200

    Update App.jsx

--- App.jsx
+++ App.jsx
@@ -3,12 +3,10 @@ import ReactDOM from 'react-dom';
 import Test from './TableState.jsx';
 
 
-console.log("hola");
+
 window.addEventListener('load', () => {
     let container = document.getElementById('TableState');
-    console.log("listener");
     if (container != null) {
-        console.log("if")
         let component = <Test/>;
         ReactDOM.render(component, container);
     }
